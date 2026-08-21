299665d8e4ffd93767b3051869546f90d5782c59
Student <student@sirafrica.org>
2026-08-21 10:34:59 +0300

    9.17 Course, step1 completed

---
+++ course/src/components/Content.tsx
@@ -0,0 +1,15 @@
+import type { ContentProps } from '../types';
+
+const Content = (props: ContentProps) => {
+  return (
+    <div>
+      {props.parts.map((part) => (
+        <p key={part.name}>
+          {part.name} {part.exerciseCount}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+export default Content;
---
+++ course/src/types.ts
@@ -0,0 +1,16 @@
+export interface CoursePart {
+  name: string;
+  exerciseCount: number;
+}
+
+export interface HeaderProps {
+  name: string;
+}
+
+export interface ContentProps {
+  parts: CoursePart[];
+}
+
+export interface TotalProps {
+  total: number;
+}
