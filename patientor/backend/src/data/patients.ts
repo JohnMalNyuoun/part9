9bcd0fe918b16f495dadec15de5a9153ef372bc2
Student <student@sirafrica.org>
2026-08-22 11:54:41 +0300

    9.25 Patientor, step 3 completed

--- patientor/backend/src/data/patients.ts
+++ patientor/backend/src/data/patients.ts
@@ -3,24 +3,61 @@ import type { Patient } from '../types.js';
 
 const patients: Patient[] = [
   {
-    id: "d2773336-2157-11e9-bf01-fe422879e639",
+    id: "d2773336-f723-11e9-8f0b-362b9e155667",
     name: "John McClane",
     dateOfBirth: "1986-07-09",
     ssn: "090786-122X",
     gender: Gender.Male,
-    occupation: "New York cop",
-    entries: [],
+    occupation: "New york city cop",
+    entries: [
+      {
+        id: "d811e46d-70b3-4d90-b090-4535c7cf8fb1",
+        date: "2015-01-02",
+        type: "Hospital",
+        specialist: "MD House",
+        diagnosisCodes: ["S62.5"],
+        description:
+          "Healing time appr. 2 weeks. patient doesn't remember how he got the injury.",
+        discharge: {
+          date: "2015-01-16",
+          criteria: "Thumb has healed.",
+        },
+      },
+    ],
   },
   {
-    id: "d2773598-2157-11e9-bf01-fe422879e639",
+    id: "d2773598-f723-11e9-8f0b-362b9e155667",
     name: "Martin Riggs",
     dateOfBirth: "1979-01-30",
     ssn: "300179-777A",
     gender: Gender.Male,
     occupation: "Cop",
+    entries: [
+      {
+        id: "fcd59fa3-e4c9-4dc0-89de-865aad1226e1",
+        date: "2019-08-05",
+        type: "OccupationalHealthcare",
+        specialist: "MD House",
+        employerName: "Hyvek",
+        diagnosisCodes: ["Z57.1", "Z74.3", "M51.2"],
+        description:
+          "Patient has recurring back pain. Given sick leave for 3 days.",
+        sickLeave: {
+          startDate: "2019-08-05",
+          endDate: "2019-08-08",
+        },
+      },
+    ],
+  },
+  {
+    id: "d2773622-f723-11e9-8f0b-362b9e155667",
+    name: "Judah Ben-Hur",
+    dateOfBirth: "1975-10-09",
+    ssn: "091075-123W",
+    gender: Gender.Male,
+    occupation: "Charioteer",
     entries: [],
   },
-  // Add entries: [] to the remaining patient objects in your data file
 ];
 
 export default patients;
--- patientor/backend/src/types.ts
+++ patientor/backend/src/types.ts
@@ -10,8 +10,51 @@ export enum Gender {
   Other = 'other',
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface Entry {}
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck';
+  healthCheckRating: HealthCheckRating;
+}
+
+export interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: Discharge;
+}
+
+export interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export interface Patient {
   id: string;
@@ -24,7 +67,4 @@ export interface Patient {
 }
 
 export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
-
-// NewPatient explicitly omits 'id' and 'entries' because the service generates 'id'
-// and initializes 'entries' as an empty array.
 export type NewPatient = Omit<Patient, 'id' | 'entries'>;
